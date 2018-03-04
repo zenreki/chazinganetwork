ee4d20e7fa0236e17192cf99abb45d040e19f8ad
zenreki <chris.hyland@zenreki.com>
2018-03-03 21:14:59 -0700

    initial

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+
+  // Require composer autoloader
+  require __DIR__ . '/vendor/autoload.php';
+
+  require __DIR__ . '/dotenv-loader.php';
+
+  use Auth0\SDK\Auth0;
+
+  $domain        = getenv('AUTH0_DOMAIN');
+  $client_id     = getenv('AUTH0_CLIENT_ID');
+  $client_secret = getenv('AUTH0_CLIENT_SECRET');
+  $redirect_uri  = getenv('AUTH0_CALLBACK_URL');
+  $audience      = getenv('AUTH0_AUDIENCE');
+
+  if($audience == ''){
+    $audience = 'https://' . $domain . '/userinfo';
+  }
+
+  $auth0 = new Auth0([
+    'domain' => $domain,
+    'client_id' => $client_id,
+    'client_secret' => $client_secret,
+    'redirect_uri' => $redirect_uri,
+    'audience' => $audience,
+    'scope' => 'openid profile',
+    'persist_id_token' => true,
+    'persist_access_token' => true,
+    'persist_refresh_token' => true,
+  ]);
+
+  $userInfo = $auth0->getUser();
+
+
+?>
+<html>
+    <head>
+        <script src="http://code.jquery.com/jquery-3.1.0.min.js" type="text/javascript"></script>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- font awesome from BootstrapCDN -->
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+
+        <link href="public/app.css" rel="stylesheet">
+
+
+
+    </head>
+    <body class="home">
+        <div class="container">
+            <div class="login-page clearfix">
+              <?php if(!$userInfo): ?>
+              <div class="login-box auth0-box before">
+                <img src="https://i.cloudup.com/StzWWrY34s.png" />
+                <h3>Auth0 Example</h3>
+                <p>Zero friction identity infrastructure, built for developers</p>
+                <a class="btn btn-primary btn-lg btn-login btn-block" href="login.php">SignIn</a>
+              </div>
+              <?php else: ?>
+              <div class="logged-in-box auth0-box logged-in">
+                <h1 id="logo"><img src="//cdn.auth0.com/samples/auth0_logo_final_blue_RGB.png" /></h1>
+                <img class="avatar" src="<?php echo $userInfo['picture'] ?>"/>
+                <h2>Welcome <span class="nickname"><?php echo $userInfo['nickname'] ?></span></h2>
+                <a class="btn btn-warning btn-logout" href="/logout.php">Logout</a>
+              </div>
+              <?php endif ?>
+            </div>
+        </div>
+    </body>
+</html>
